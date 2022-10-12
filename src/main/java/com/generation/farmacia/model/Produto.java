94f10e9af1c9f37f0f10448bf0ebb3f6cc73d2d9
belaxsantos <isabela2088@hotmail.com>
2022-10-11 23:47:48 -0300

    Crud Farmacia

---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,56 @@
+package com.generation.farmacia.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "O atributo descrição é obrigatório")
+	private String descricao;
+	
+	@OneToMany(mappedBy = "produto", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("produto")
+	private List<Categoria> categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Categoria> getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(List<Categoria> categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+}
